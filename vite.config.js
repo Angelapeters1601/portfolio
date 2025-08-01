bb859dc2b5e1f40a249419543ec7d043b6b135b6
unknown <nwattaha@gmail.com>
2025-08-01 15:37:24 +0100

    fix: vercel build issues

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
-  server: {
-    historyApiFallback: true,
-  },
 });
