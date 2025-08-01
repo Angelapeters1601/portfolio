841f203ead0a492b3bd6ce3c5358d679f19ba1f5
unknown <nwattaha@gmail.com>
2025-08-01 15:21:12 +0100

    fix deployment bug

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -8,6 +9,11 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
   },
   server: {
     historyApiFallback: true,
